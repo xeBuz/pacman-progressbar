80611e4adb05ae194c74a616dfa16b56a865d407
Jesus Roldan <xebuz18@gmail.com>
2012-10-21 03:44:17 -0300

    PEP8 fixes

--- PacmanProgressbar.py
+++ PacmanProgressbar.py
@@ -4,15 +4,18 @@ import sys
 import os
 import itertools
 
-MARGIN  = 3
+MARGIN = 3
 DEFAULT_WIDTH = int(os.popen('stty size', 'r').read().split()[1]) - MARGIN
 
- 
+PACMAN = ["\033[1;33mC\033[m", "\033[1;33mc\033[m"]
+CANDY = ["\033[0;37mo\033[m", "\033[0;37m \033[m", "\033[0;37m \033[m"]
+
+
 class Pacman():
-    """ PacMan Progress Bar: 
+    """ PacMan Progress Bar:
     Parameters:
         Start: It should be 0
-        End: Defines the dimension of the bar in an amount of items or steps, Default 100
+        End: Defines the dimension of the bar in an amount of items or steps
         Width: Size (in chars) of the bar. Default = Console Size
         Step: Current position in the progrressbar, Default 0
     """
@@ -31,8 +34,8 @@ class Pacman():
             self.width = DEFAULT_WIDTH
 
         self.bar = "-"
-        self.pacman = itertools.cycle(["\033[1;33mC\033[m", "\033[1;33mc\033[m"])
-        self.candy =  itertools.cycle(["\033[0;37mo\033[m","\033[0;37m \033[m","\033[0;37m \033[m"])
+        self.pacman = itertools.cycle(PACMAN)
+        self.candy = itertools.cycle(CANDY)
         self.candybar = [None] * self.width
 
         for i in range(self.width):
@@ -42,17 +45,17 @@ class Pacman():
             self.__write(self.candybar[i])
         self.__write("]")
 
-    def update(self, value = 1):
-        """ Update the progress in the bar 
-            Parameter: value, is the incresing size of the bar. By default is 1.
+    def update(self, value=1):
+        """ Update the progress in the bar
+            Parameter: value, is the incresing size of the bar. By default 1
         """
         self.step = self.step + value
         self.__draw()
 
     def progress(self, value):
-        """ Set the progress in the bar 
-            Parameter: value, is the specify size of the bar. No default value.
-        """        
+        """ Set the progress in the bar
+            Parameter: value, is the specify size of the bar. No default value
+        """
         self.step = value
         self.__draw()
 
@@ -60,24 +63,20 @@ class Pacman():
         sys.stdout.buffer.write(bytes(value, encode))
 
     def __set_percentage(self):
-        self.percentage = format((100 * float(self.step)/float(self.end)),'.1f')
+        self.percentage = format((100 * float(self.step) / float(self.end)), '.1f')
 
     def __draw(self):
-        
-        self.__set_percentage()        
 
-        porc = "\r"+ str(self.percentage)+"%["
-        pos = (((self.step/ (self.end - self.start) * 100) * (self.width - len(porc)))/ 100 )
-        
-        self.__write(porc)     
+        self.__set_percentage()
+
+        porc = "\r" + str(self.percentage) + "%["
+        pos = (((self.step / (self.end - self.start) * 100) * (self.width - len(porc))) / 100)
+
+        self.__write(porc)
 
         for i in range(int(pos)):
             self.__write(self.bar)
 
-        self.__write(next(self.pacman))       
-        
+        self.__write(next(self.pacman))
+
         sys.stdout.flush()
-       
-
-
-
