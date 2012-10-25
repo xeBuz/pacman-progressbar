aa6603de40a54319b1f44b78b338d5f4d47f8490
Jesus Roldan <xebuz18@gmail.com>
2012-10-25 15:56:40 -0300

    New examples

---
+++ simple_example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import time
+from pacmanprogressbar import Pacman
+
+
+if __name__ == "__main__":
+
+    p = Pacman(end=100, text="Progress")
+    for x in range(p.len):
+        p.update()
+        time.sleep(.1)
+        
+    print("Finished")
